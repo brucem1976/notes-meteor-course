bdd787514810b77503042f9bbbc0e7e94aec2e6b
Bruce Martin <bruceandnatasha@gmail.com>
2017-07-19 10:06:20 +0200

    Added selectable notes and associated url changes

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+  return (
+    <div>
+      <h5>You have no notes</h5>
+      <p>Create your first note to see it here.</p>
+    </div>
+  );
+};
+
+export default NoteListEmptyItem;
